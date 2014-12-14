aa6629eab8df987300ba20b904ad13048b9889a1
David Lewis <david.lewis@hiveempire.com>
2014-12-15 09:52:01 +1100

    Add init call for extra providers

---
+++ tests/customproviders.tests.js
@@ -0,0 +1,33 @@
+/*global describe, it*/
+describe('Custom Providers', function () {
+  it('should get data from a custom provider', function (done) {
+    var customProviders, embed;
+
+    // Use the custom provider provided in the documentation
+    customProviders = {
+      "vimeo.com": {
+        init: function (urlStr) {
+          return {
+            getUrls: function () {
+              return {
+                embed: "http://vimeo.com/api/oembed.json?url=" + urlStr,
+                video: urlStr
+              };
+            }
+          };
+        }
+      }
+    };
+
+    embed = require('../index').init(customProviders);
+
+    embed.get({
+      url: "https://vimeo.com/62584176"
+    }, function (err, result) {
+      result.provider_name.should.eql('Vimeo');
+      result.provider_url.should.eql('https://vimeo.com/');
+      result.video_url.should.eql('https://vimeo.com/62584176');
+      done(err);
+    });
+  });
+});
